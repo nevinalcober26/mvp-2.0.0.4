ebdcd2b7773d7f1f94bca9390f74387df880098c
Nevin Alcober <85alcober@gmail.com>
2026-05-20 12:53:35 +0000

    not fixed but sticky on top position!!!! as user scroll it will stick on

--- src/app/dashboard/operations/order-hub/page.tsx
+++ src/app/dashboard/operations/order-hub/page.tsx
@@ -433,7 +433,7 @@ export default function OrderHubPage() {
         </div>
       </div>
 
-      <main className="flex-1 overflow-hidden p-6 flex gap-6">
+      <main className="flex-1 p-6 flex gap-6">
         <div className="flex-1 flex gap-6 min-w-0 pr-80">
           {columns.map((col) => {
             const columnOrders = getFilteredStatusOrders(col.id);
@@ -469,8 +469,8 @@ export default function OrderHubPage() {
           })}
         </div>
 
-        {/* FIXED POSITION SIDEBAR */}
-        <aside className="fixed top-[220px] right-6 w-80 hidden xl:flex flex-col gap-6 text-left z-30">
+        {/* STICKY MANAGEMENT SIDEBAR */}
+        <aside className="sticky top-6 self-start w-80 hidden xl:flex flex-col gap-6 text-left z-30">
           <Card className="border shadow-sm bg-white overflow-hidden flex flex-col rounded-2xl h-[500px]">
             <CardHeader className="bg-slate-900 text-white p-6 shrink-0">
               <div className="flex items-center justify-between mb-1">
